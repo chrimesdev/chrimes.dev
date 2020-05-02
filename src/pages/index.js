f39b76a521bfa6d39a97678b31c0fc3e5eca3756
AdamChrimes <iChrimes@live.co.uk>
2020-05-02 12:29:19 +0100

    :tada: initial application commit

---
+++ src/pages/index.js
@@ -0,0 +1,44 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout, { siteTitle } from '../shared/layout'
+import Date from '../shared/date'
+import { getSortedPostsData } from '../lib/posts'
+import utilStyles from '../styles/utils.module.scss'
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Senior Developer at NHS Digital, working on the <a href="https://www.nhs.uk">NHS website</a> and <a href="https://service-manual.nhs.uk">NHS digital service manual</a>.</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
