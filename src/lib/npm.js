4313346397ec49bda16de1c3fb7bf7eb71d26048
AdamChrimes <iChrimes@live.co.uk>
2020-05-15 10:37:15 +0100

    get npm stats from nhsuk-frontend

---
+++ src/lib/npm.js
@@ -0,0 +1,27 @@
+export default async () => {
+  try {
+    // Get an array of releases from GitHub API
+    const githubRes = await fetch('https://api.github.com/repos/nhsuk/nhsuk-frontend/releases');
+    const releases = await githubRes.json();
+    // Build a new array of promises
+    const promises = releases.map(async (release, index) => {
+      // Get the next release 
+      const nextRelease = releases[index-1];
+      // Get when the next release was published - return todays date if the release is the current release
+      const end = nextRelease ? nextRelease.published_at : new Date().toISOString();
+      // Get data from NPM API based off release published date and the next release's published date 
+      const npmRes = await fetch(`https://api.npmjs.org/downloads/point/${release.published_at.split('T')[0]}:${end.split('T')[0]}/nhsuk-frontend`);
+      // Extract the downloads from NPM API response with an alias of npmDownloads
+      const { downloads: npmDownloads } = await npmRes.json();
+      // Return original release object with added npmDownloads
+      return {
+        ...release,
+        npmDownloads,
+      };
+    });
+    // Return promises array once all async processes are complete
+    return await Promise.all(promises);
+  } catch (error) {
+    throw Error(error);
+  }
+};
--- src/pages/github.js
+++ src/pages/github.js
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Date from '../shared/date'
-import Layout, { siteTitle } from '../shared/layout'
-import utilStyles from '../styles/utils.module.scss'
+import Head from 'next/head';
+import Date from '../shared/date';
+import Layout, { siteTitle } from '../shared/layout';
+import downloadsData from '../lib/npm';
 
 export default function GitHub({ releases }) { 
   return (
@@ -12,9 +12,9 @@ export default function GitHub({ releases }) {
       <section>
         <h1>NHS.UK frontend releases</h1>
         <ul>
-        {releases.map(({ id, name, html_url, published_at, assets }) => (
+        {releases.map(({ id, name, html_url, published_at, assets, npmDownloads }) => (
           <li key={id}>
-            <a href={html_url}>{name}</a> (<Date dateString={published_at} />) - {assets[0].download_count} downloads (<a href={assets[0].browser_download_url}>Download release</a>)
+            <a href={html_url}>{name}</a> (<Date dateString={published_at} />) - {assets[0].download_count} downloads on GitHub and {npmDownloads} npm installs
           </li>
         ))}
         </ul>
@@ -27,14 +27,14 @@ export default function GitHub({ releases }) {
 // It won't be called on client-side, so you can even do
 // direct database queries. See the "Technical details" section.
 export async function getStaticProps() {
-  const res = await fetch('https://api.github.com/repos/nhsuk/nhsuk-frontend/releases')
-  const releases = await res.json()
-
-  // By returning { props: posts }, the Blog component
-  // will receive `posts` as a prop at build time
-  return {
-    props: {
-      releases,
-    },
+  try {
+    const releases = await downloadsData();
+    return {
+      props: {
+        releases,
+      },
+    }
+  } catch (error) {
+    throw Error(error);
   }
 }
