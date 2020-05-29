218e790dffbdc4729c4328b1805e5783cdbdaa9f
AdamChrimes <iChrimes@live.co.uk>
2020-05-29 14:06:39 +0100

    put the release data in a data table

--- src/pages/releases/index.js
+++ src/pages/releases/index.js
@@ -12,13 +12,26 @@ export default function Releases({ repo, releases }) {
       </Head>
       <section>
         <h1><a href={repo.html_url}>{repo.name}</a> releases</h1>
-        <ul>
-        {releases.map(({ id, name, html_url, published_at, assets, npmDownloads }) => (
-          <li key={id}>
-            <a href={html_url}>{name}</a> (<Date dateString={published_at} />) - {assets[0].download_count} downloads on GitHub and <Number numberString={npmDownloads} /> npm installs
-          </li>
-        ))}
-        </ul>
+
+        <table>
+          <caption>{repo.name} telemetry data</caption>
+          <tbody>
+            <tr>
+              <th scope="col">Version</th>
+              <th scope="col">Release date</th>
+              <th scope="col">GitHub downloads</th>
+              <th scope="col">npm installs</th>
+            </tr>
+            {releases.map(({ id, name, html_url, published_at, assets, npmDownloads }) => (
+            <tr key={id} class="table-content">
+              <th scope="row"><a href={html_url}>{name}</a></th>
+              <td><Date dateString={published_at} /></td>
+              <td>{assets[0].download_count}</td>
+              <td><Number numberString={npmDownloads} /></td>
+            </tr>
+            ))}
+          </tbody>
+        </table>
       </section>
     </Layout>
   )
